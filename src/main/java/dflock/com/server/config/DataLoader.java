19ae504e80bde456d4bf66c6b90596d939ab7b29
cadi <dbtmdgns4296@naver.com>
2025-05-07 23:08:40 +0900

    add mock id logic

--- src/main/java/dflock/com/server/config/DataLoader.java
+++ src/main/java/dflock/com/server/config/DataLoader.java
@@ -25,8 +25,18 @@ public class DataLoader {
             ObjectMapper mapper = new ObjectMapper();
             InputStream is = getClass().getResourceAsStream("/static/items.json");
             List<Item> items = mapper.readValue(is, new TypeReference<List<Item>>() {});
-            System.out.println(items);
-            itemRepository.saveAll(items);
+            System.out.println("items" +  items);
+            // ID가 없는 경우 자동 생성하도록 설정
+            final int[] cnt = {0};
+            List<Item> processedItems = items.stream()
+                    .peek(item -> {
+                        if (item.getId() == null) {
+                            item.setId(String.valueOf(cnt[0])); // 명시적으로 ID를 비워서 자동 생성하도록
+                            cnt[0] += 1;
+                        }
+                    })
+                    .toList();
+            itemRepository.saveAll(processedItems);
             System.out.println("✅ 초기 아이템 데이터 로드 완료");
         } catch (Exception e) {
             System.err.println("❌ 초기 데이터 로드 실패: " + e.getMessage());
